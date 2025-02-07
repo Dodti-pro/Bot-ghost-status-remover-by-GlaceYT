d31bc59f0b3492eaec275de3b944cfe958f232b1
Dodti-pro <yoyoswag2024@gmail.com>
2025-02-07 19:13:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Listening to Rado Gaming", "Playing Whiltelist", "Watching 300+ members"];
+const statusMessages = ["Listening to **Rado Gaming**", "Playing **whitelist**", "Watching **300+ members**"];
 const statusTypes = [ 'dnd', 'idle', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
